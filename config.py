3bf33f1aed5704a501c4339d026642c3ece85f8c
Olya Dusheba <74534204+Dusheba@users.noreply.github.com>
2021-05-28 13:55:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '1655214869:AAFvcjbOx8mT-8uK5_Htc2EjNEMQeaBP8GQ'
-users=['449188943','833364491']
+token = 'write your token here'
+users=['write user id here']
 lastimage='/var/lib/motion/lastsnap.jpg'
 motiondir='/var/lib/motion'
